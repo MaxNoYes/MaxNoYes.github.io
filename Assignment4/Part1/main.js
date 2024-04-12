4ec0fbeb260d4998859b50b018d7f2e3f1fdc676
RelicShield <158072769+MaxNoYes@users.noreply.github.com>
2024-04-12 14:07:28 -0400

    Finally fixed it

--- Assignment4/Part1/main.js
+++ Assignment4/Part1/main.js
@@ -50,9 +50,29 @@ function randomValueFromArray(array){
       newStory = newStory.replace('300 pounds', weight)
 
     }
+    else{
+      var xItem = randomValueFromArray(insertX);
+      var yItem = randomValueFromArray(insertY);
+      var zItem = randomValueFromArray(insertZ);
+
+      newStory = newStory.replace(':insertx:', xItem);
+      newStory = newStory.replace(':inserty:', yItem);
+      newStory = newStory.replace(':insertz:', zItem);
+      //divide by 14
+      //(F − 32) × 5/9 = C
+
+    }
+
+    if(document.getElementById("uk").checked) {
+      const weight = Math.round(300/14) + ' stone';
+      const temperature =  Math.round(((94 -32) * (5/9))) + ' centigrade';
+      newStory = newStory.replace('94 fahrenheit', temperature)
+      newStory = newStory.replace('300 pounds', weight)
+    }
 
     story.textContent = newStory;
     story.style.visibility = 'visible';
   }
+
 
   randomize.addEventListener('click', result);
